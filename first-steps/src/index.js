ec6097dad32d03f6dd980cf1104d8aa68786ef91
guitakashiba <takashiba30@gmail.com>
2025-05-28 19:36:02 -0300

    Feat: add new variable for filtering young heroes

--- first-steps/src/index.js
+++ first-steps/src/index.js
@@ -8,6 +8,9 @@ async function demoFinds(){
 
     const allHeroes = await repo.find();
     console.log('All heroes: ', allHeroes)
+
+    const youngHeroes = (await repo.find()).filter(y => y.age >= 35);
+    console.log('Young Heroes: ', youngHeroes)
 }
 
 demoFinds().catch(err => console.error(err));
